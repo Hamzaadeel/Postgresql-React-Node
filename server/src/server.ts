8622d4874fdec51587bc5b86ca6c8edf488be452
Hamza Adeel <mhamza2101@gmail.com>
2025-04-28 11:22:27 +0500

    Server ip update

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,7 +35,7 @@ const server = createServer(app);
 // Initialize Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://13.218.202.231:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -79,7 +79,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://13.218.202.231:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
